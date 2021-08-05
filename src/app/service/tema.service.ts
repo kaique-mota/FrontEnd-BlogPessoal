95887dc94ae6832a9ef3249f42e219547398cc76
kaique-mota <kaiquecarvalho42724@gmail.com>
2021-08-04 21:29:03 -0300

    implementação do get e post de temas

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,28 @@
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+     return this.http.get<Tema[]>('https://mundinhokaiquemota.herokuapp.com/tema', this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('https://mundinhokaiquemota.herokuapp.com/tema', tema, this.token)
+  }
+
+
+}
